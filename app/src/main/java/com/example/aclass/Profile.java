bb52fa15ce7d87a4e6de3d890acd13c6720cd268
WOOSUNG-AHN <dnlsxjfj145>
2020-01-16 16:29:47 +0900

    2020.01.16 내정보

--- app/src/main/java/com/example/aclass/Profile.java
+++ app/src/main/java/com/example/aclass/Profile.java
@@ -1,6 +1,9 @@
 package com.example.aclass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 import android.os.Bundle;
 
@@ -9,6 +12,6 @@ public class Profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mypage);
+        setContentView(R.layout.mypage);
     }
 }
